591f8af3216f44620f68de8046533028525a9fca
ranzyn <rana_zayan@yahoo.com>
2022-09-08 16:13:59 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* main - Entry point 
+ *
+ * Return: Always after printing the function
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
