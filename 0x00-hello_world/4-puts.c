41ea67a5d69f6503a16ddf5e2d7413e7515081ea
ranzyn <rana_zayan@yahoo.com>
2022-09-08 16:15:55 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@
  * Return: Always after printing the function
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
